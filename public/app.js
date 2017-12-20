713258248f9fad8a58fd2e50b6bef4b46eb87283
ddigiulio <ddigiulio90@gmail.com>
2017-12-20 15:44:53 -0500

    final

--- public/app.js
+++ public/app.js
@@ -203,40 +203,9 @@ function initAutocomplete() {
         marker.setPosition(place.geometry.location);
         marker.setVisible(true);
 
-        // var address = '';
-        // if (place.address_components) {
-        //     address = [
-        //         (place.address_components[0] && place.address_components[0].short_name || ''),
-        //         (place.address_components[1] && place.address_components[1].short_name || ''),
-        //         (place.address_components[2] && place.address_components[2].short_name || '')
-        //     ].join(' ');
-        // }
-
-        // infowindowContent.children['place-icon'].src = place.icon;
-        // infowindowContent.children['place-name'].textContent = place.name;
-        // infowindowContent.children['place-address'].textContent = address;
-        // infowindow.open(map, marker);
     });
 
-    // Sets a listener on a radio button to change the filter type on Places
-    // Autocomplete.
-    // function setupClickListener(id, types) {
-    //   var radioButton = document.getElementById(id);
-    //   radioButton.addEventListener('click', function() {
-    //     autocomplete.setTypes(types);
-    //   });
-    // }
 
-    // setupClickListener('changetype-all', []);
-    // setupClickListener('changetype-address', ['address']);
-    // setupClickListener('changetype-establishment', ['establishment']);
-    // setupClickListener('changetype-geocode', ['geocode']);
-
-    // document.getElementById('use-strict-bounds')
-    //     .addEventListener('click', function() {
-    //       console.log('Checkbox clicked! New state=' + this.checked);
-    //       autocomplete.setOptions({strictBounds: this.checked});
-    //     });
 
 
 }
